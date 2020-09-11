5c93ab4f9df558172e7607c2a2d960abae891490
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-09-11 18:08:49 -0500

    Updates

---
+++ precache-manifest.91ffe38a4f262698f17c803a9124ad40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7083b5d184056098257f5d90bb0ce9a5",
+    "url": "/KonectaBlog/index.html"
+  },
+  {
+    "revision": "dc5156170212c4ab6d00",
+    "url": "/KonectaBlog/static/css/main.8aabb8e0.chunk.css"
+  },
+  {
+    "revision": "445c37f1f4a7caaf1048",
+    "url": "/KonectaBlog/static/js/2.3e27f59f.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/KonectaBlog/static/js/2.3e27f59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5156170212c4ab6d00",
+    "url": "/KonectaBlog/static/js/main.7902b93b.chunk.js"
+  },
+  {
+    "revision": "651742ab44e075a18b82",
+    "url": "/KonectaBlog/static/js/runtime-main.c2178b05.js"
+  }
+]);
